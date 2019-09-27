bdb601856aaa07114bb527051c46869e3c9b8856
Nat <nat.jnava@gmail.com>
2019-09-26 20:10:17 -0700

    pls

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const addButton = document.getElementById('addButton')
+const num1 = document. getElementById('num1')
+const num2 = document. getElementById('num2')
+const output = document.getElementById('output')
 
 addButton.addEventListener('click', addButtonClicked)
 
 function addButtonClicked() {
-    const numb1Input = document.getElementById('num')
+    const numb1Input = document.getElementById('num1')
     const numb1String = numb1Input.value
     const number1 = parseInt(num1String, 16)
 
     const numb2Input = document.getElementById('num2')
-    const numb1String = numb1Input.value
+    const numb2String = numb2Input.value
     const number2 = parseInt(num2String, 16)
 
     const sum = number1 + number2

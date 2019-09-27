6de6e142d646b4eb95427228bda8b604c3302f85
Nat <nat.jnava@gmail.com>
2019-09-26 19:59:17 -0700

    next

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const addButton = document.getElementById('addButton')
 
-addButton.addEventListener('click', onClicked)
+addButton.addEventListener('click', addButtonClicked)
 
-function onClicked() {
-    alert("you clicked on the button")
+function addButtonClicked() {
+    const numb1Input = document.getElementById('num')
+    const numb1String = numb1Input.value
+    const number1 = parseInt(num1String, 16)
+
+    const numb2Input = document.getElementById('num2')
+    const numb1String = numb1Input.value
+    const number2 = parseInt(num2String, 16)
+
+    const sum = number1 + number2
+    const outputDiv = document.getElementById('Output')
+    outputDiv.innerHTML = "The sum of two numbers is" + sum
 }
